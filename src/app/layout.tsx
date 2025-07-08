304dfa97b9ea67c9df9f0a50a7279cdf8e0f6ef7
anquetil <25070051+anquetil@users.noreply.github.com>
2025-07-08 16:55:51 -0700

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -145,7 +145,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-        <GoogleTagManager gtmId="G-PTDJ50YTYC" />
+        <GoogleAnalytics gaId="G-PTDJ50YTYC" />
       <head>
         <script
           type="application/ld+json"
